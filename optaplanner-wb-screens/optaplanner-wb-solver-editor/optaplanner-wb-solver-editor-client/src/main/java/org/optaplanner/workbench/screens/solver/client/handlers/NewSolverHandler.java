21ae510fdf30eca4f13c9bd837ef128c06bb8531
Toni Rikkola <toni@rikkola.net>
2016-02-04 11:44:52 +0200

    PLANNER-516: Workbench: Solver config editor needs validate button that builds the Solver.
    (cherry picked from commit 5d55b504d8e8d47320328b04ed5b792b67263332)

--- optaplanner-wb-screens/optaplanner-wb-solver-editor/optaplanner-wb-solver-editor-client/src/main/java/org/optaplanner/workbench/screens/solver/client/handlers/NewSolverHandler.java
+++ optaplanner-wb-screens/optaplanner-wb-solver-editor/optaplanner-wb-solver-editor-client/src/main/java/org/optaplanner/workbench/screens/solver/client/handlers/NewSolverHandler.java
@@ -22,6 +22,8 @@ import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.IsWidget;
 import org.guvnor.common.services.project.model.Package;
 import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.security.shared.api.RoleImpl;
+import org.jboss.errai.security.shared.api.identity.User;
 import org.kie.workbench.common.widgets.client.handlers.DefaultNewResourceHandler;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcePresenter;
 import org.kie.workbench.common.widgets.client.resources.i18n.CommonConstants;
@@ -30,7 +32,6 @@ import org.optaplanner.workbench.screens.solver.client.resources.i18n.SolverEdit
 import org.optaplanner.workbench.screens.solver.client.type.SolverResourceType;
 import org.optaplanner.workbench.screens.solver.model.SolverConfigModel;
 import org.optaplanner.workbench.screens.solver.service.SolverEditorService;
-import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.ext.widgets.common.client.callbacks.HasBusyIndicatorDefaultErrorCallback;
 import org.uberfire.ext.widgets.common.client.common.BusyIndicatorView;
 import org.uberfire.workbench.type.ResourceTypeDefinition;
@@ -39,19 +40,30 @@ import org.uberfire.workbench.type.ResourceTypeDefinition;
  * Handler for the creation of new Solver Configurations
  */
 @ApplicationScoped
-public class NewSolverHandler extends DefaultNewResourceHandler {
+public class NewSolverHandler
+        extends DefaultNewResourceHandler {
 
-    @Inject
     private Caller<SolverEditorService> solverService;
 
-    @Inject
-    private PlaceManager placeManager;
-
-    @Inject
     private SolverResourceType resourceType;
 
-    @Inject
     private BusyIndicatorView busyIndicatorView;
+
+    private User user;
+
+    public NewSolverHandler() {
+    }
+
+    @Inject
+    public NewSolverHandler( final Caller<SolverEditorService> solverService,
+                             final SolverResourceType resourceType,
+                             final BusyIndicatorView busyIndicatorView,
+                             final User user ) {
+        this.solverService = solverService;
+        this.resourceType = resourceType;
+        this.busyIndicatorView = busyIndicatorView;
+        this.user = user;
+    }
 
     @Override
     public String getDescription() {
@@ -81,4 +93,8 @@ public class NewSolverHandler extends DefaultNewResourceHandler {
                                                                                                     "" );
     }
 
+    @Override
+    public boolean canCreate() {
+        return user.getRoles().contains( new RoleImpl( "plannermgmt" ) );
+    }
 }
