76e1ccfff2450063c7dbc667b5216912b54c7683
Michael Anstis <michael.anstis@gmail.com>
2017-05-25 14:05:26 +0100

    UF-517: Bootstrap Selector CSS overriding Patternfly Selector CSS

--- optaplanner-wb-screens/optaplanner-wb-domain-editor/optaplanner-wb-domain-editor-client/src/main/java/org/optaplanner/workbench/screens/domaineditor/client/DomainEditorEntryPoint.java
+++ optaplanner-wb-screens/optaplanner-wb-domain-editor/optaplanner-wb-domain-editor-client/src/main/java/org/optaplanner/workbench/screens/domaineditor/client/DomainEditorEntryPoint.java
@@ -1,10 +1,16 @@
 package org.optaplanner.workbench.screens.domaineditor.client;
 
+import org.jboss.errai.ioc.client.api.AfterInitialization;
 import org.jboss.errai.ioc.client.api.EntryPoint;
 import org.jboss.errai.ui.shared.api.annotations.Bundle;
+import org.uberfire.client.views.pfly.sys.PatternFlyBootstrapper;
 
 @EntryPoint
 @Bundle("resources/i18n/DomainEditorConstants.properties")
 public class DomainEditorEntryPoint {
 
+    @AfterInitialization
+    public void startApp() {
+        PatternFlyBootstrapper.ensureBootstrapSelectIsAvailable();
+    }
 }
