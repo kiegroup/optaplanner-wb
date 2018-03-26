065890613e2612278be649ab89ab7933bd2af20a
Jiří Locker <jiri.locker@gmail.com>
2018-03-26 17:20:46 +0200

    PLANNER-1026 [ui] Copy original optaplanner-wb-webapp

---
+++ optaplanner-wb-ui/src/main/java/org/optaplanner/workbench/client/resources/i18n/AppConstants.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.workbench.client.resources.i18n;
+
+import org.jboss.errai.ui.shared.api.annotations.TranslationKey;
+
+public interface AppConstants {
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_AdministrationPerspective = "AdministrationPerspective.AdministrationPerspective";
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_MenuExplore = "AdministrationPerspective.MenuExplore";
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_MenuExploreFiles = "AdministrationPerspective.MenuExploreFiles";
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_MenuRepositories = "AdministrationPerspective.MenuRepositories";
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_MenuListRepositories = "AdministrationPerspective.MenuListRepositories";
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_MenuCloneRepository = "AdministrationPerspective.MenuCloneRepository";
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_MenuNewRepository = "AdministrationPerspective.MenuNewRepository";
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_MenuOrganizationalUnits = "AdministrationPerspective.MenuOrganizationalUnits";
+
+    @TranslationKey(defaultValue = "")
+    String AdministrationPerspective_MenuManageOrganizationalUnits = "AdministrationPerspective.MenuManageOrganizationalUnits";
+
+    @TranslationKey(defaultValue = "")
+    String HomeProducer_Heading = "HomeProducer.Heading";
+
+    @TranslationKey(defaultValue = "")
+    String HomeProducer_SubHeading = "HomeProducer.SubHeading";
+
+    @TranslationKey(defaultValue = "")
+    String HomeProducer_Design = "HomeProducer.Design";
+
+    @TranslationKey(defaultValue = "")
+    String HomeProducer_DesignDescription = "HomeProducer.DesignDescription";
+
+    @TranslationKey(defaultValue = "")
+    String HomeProducer_DevOps = "HomeProducer.DevOps";
+
+    @TranslationKey(defaultValue = "")
+    String HomeProducer_DevOpsDescription = "HomeProducer.DevOpsDescription";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_Administration = "OptaPlannerWorkbenchEntryPoint.Administration";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_Home = "OptaPlannerWorkbenchEntryPoint.Home";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_Authoring = "OptaPlannerWorkbenchEntryPoint.Authoring";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_ProjectAuthoring = "OptaPlannerWorkbenchEntryPoint.ProjectAuthoring";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_Settings = "OptaPlannerWorkbenchEntryPoint.Settings";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_Library = "OptaPlannerWorkbenchEntryPoint.Library";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_ArtifactRepository = "OptaPlannerWorkbenchEntryPoint.ArtifactRepository";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_Deploy = "OptaPlannerWorkbenchEntryPoint.Deploy";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_Admin = "OptaPlannerWorkbenchEntryPoint.Admin";
+
+    @TranslationKey(defaultValue = "")
+    String OptaPlannerWorkbenchEntryPoint_ExecutionServers = "OptaPlannerWorkbenchEntryPoint.ExecutionServers";
+
+    @TranslationKey(defaultValue = "")
+    String M2RepoPerspective_Upload = "M2RepoPerspective.Upload";
+
+    @TranslationKey(defaultValue = "")
+    String M2RepoPerspective_Refresh = "M2RepoPerspective.Refresh";
+}
